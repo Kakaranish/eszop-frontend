486aa7171dddabc74a9c898ddac7c312595f014a
Kakaranish <stasiekgruz@gmail.com>
2021-02-16 22:17:35 +0100

    + SignIn page refactor

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { authorizedRequestHandler, getFormDataJsonFromEvent, requestHandler } from 'common/utils';
 import AwareComponentBuilder from 'common/AwareComponentBuilder';
+import { toast } from 'react-toastify';
 
 const SignInPage = (props) => {
 
@@ -15,42 +16,87 @@ const SignInPage = (props) => {
 
         const signInUri = "/identity-api/auth/sign-in";
         const signInAction = async () => await axios.post(signInUri, formDataJson);
-        await requestHandler(signInAction);
+        await requestHandler(signInAction,
+            {
+                status: 200,
+                callback: () => { }
+            },
+            {
+                status: -1,
+                callback: result => {
+                    toast.warn(`Error ${result.status}`);
+                    history.push('/refresh');
+                }
+            }
+        );
 
         const getMeUri = "/identity-api/user/me";
         const getMeAction = async () => await axios.get(getMeUri);
-        await requestHandler(getMeAction, {
-            status: 200,
-            callback: async getMeResult => {
-                props.setIdentity(getMeResult);
+        await requestHandler(getMeAction,
+            {
+                status: 200,
+                callback: async getMeResult => {
+                    props.setIdentity(getMeResult);
+                }
+            },
+            {
+                status: -1,
+                callback: result => {
+                    toast.error(`Error ${result.status}. Please sign out and try again...`);
+                    history.push('/refresh');
+                }
             }
-        });
+        );
 
         const getCartUri = `/carts-api/cart`;
         const getCartAction = async () => await axios.get(getCartUri);
 
-        authorizedRequestHandler(getCartAction, {
-            status: 200,
-            callback: result => {
-                result.cartItems.forEach(cartItem => props.addOrUpdateCartItem(cartItem));
-                history.push('/');
+        authorizedRequestHandler(getCartAction,
+            {
+                status: 200,
+                callback: result => {
+                    result.cartItems.forEach(cartItem => props.addOrUpdateCartItem(cartItem));
+                    history.push('/');
+                }
+            },
+            {
+                status: -1,
+                callback: result => {
+                    toast.error(`Error ${result.status}. Please sign out and try again...`);
+                    history.push('/refresh');
+                }
             }
-        });
+        );
     };
 
     return <>
-        <form onSubmit={onSubmit}>
-            <div className="form-group">
-                <input name="email" type="email" className="form-control" id="emailInput" placeholder="Email..." required />
-            </div>
-            <div className="form-group">
-                <input name="password" type="password" className="form-control" id="passwordInput" placeholder="Password..." required />
-            </div>
+        <div className="offset-md-2 col-md-8 col-12">
+            <h4 className="mb-3">Sign in to your account</h4>
 
-            <button type="submit" className="btn btn-primary">
-                Sign In
-            </button>
-        </form>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input name="email" type="email" className="form-control"
+                        id="emailInput" placeholder="Email..." required
+                    />
+                </div>
+                <div className="form-group mb-1">
+                    <input name="password" type="password" className="form-control"
+                        id="passwordInput" placeholder="Password..." required
+                    />
+                </div>
+
+                <div className="text-secondary mb-3">
+                    You have no account? No problem!&nbsp;
+                    <Link to='/auth/sign-up'>
+                        Create it
+                    </Link>
+                </div>
+
+                <button type="submit" className="btn btn-block btn-outline-success">
+                    Sign In
+                </button>
+            </form>
+        </div>
     </>
 }
 
