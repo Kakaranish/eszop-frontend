9eb9005f0fcf1be00461777e9c8f652b3b5d1c81
Kakaranish <stasiekgruz@gmail.com>
2021-02-27 14:32:20 +0100

    + implemented FillDeliveryInfoPage

---
+++ src/pages/User/Orders/FillDeliveryInfoPage/DeliveryMethodsSection.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const DeliveryMethodsSection = (props) => {
+
+    const { deliveryMethods, defaultSelected } = props;
+
+    const [selectedMethod, setSelectedMethod] = useState(defaultSelected);
+
+    const onSelectionChange = event => setSelectedMethod(event.target.value);
+
+    return <div className="mt-4">
+        <h4 className="mb-3">Choose delivery method</h4>
+
+        <div className="form-check">
+            {
+                deliveryMethods.map((deliveryMethod, index) => {
+                    return <div key={index} className="form-check pl-0">
+                        <input
+                            id={`method-input-${index}`}
+                            type="radio"
+                            name="deliveryMethodName"
+                            className="form-check-input"
+                            value={deliveryMethod.name}
+                            checked={selectedMethod === deliveryMethod.name}
+                            onChange={onSelectionChange}
+                        />
+
+                        <label className="form-check-label" htmlFor={`method-input-${index}`}>
+                            {deliveryMethod.name} -&nbsp;
+                            {deliveryMethod.price.toFixed(2)} PLN
+                        </label>
+                    </div>
+                })
+            }
+        </div>
+    </div>
+};
+
+export default DeliveryMethodsSection;
---
+++ src/pages/User/Orders/FillDeliveryInfoPage/FillDeliveryInfoPage.js
@@ -0,0 +1,97 @@
+import axios from 'axios';
+import { authorizedRequestHandler, getFormDataJsonFromEvent } from 'common/utils';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import DeliveryAddressesModal from './DeliveryAddressesModal';
+import DeliveryMethodsSection from './DeliveryMethodsSection';
+import OrderDeliveryAddressForm from './OrderDeliveryAddressForm';
+
+const FillDeliveryInfoPage = (props) => {
+
+    const orderId = props.match.params.id;
+
+    const history = useHistory();
+
+    const [state, setState] = useState({
+        loading: true,
+        deliveryMethods: null
+    });
+
+    const [deliveryAddress, setDeliveryAddress] = useState({
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+        country: "",
+        zipCode: "",
+        city: "",
+        street: ""
+    });
+
+    useEffect(() => {
+        const fetch = async () => {
+            const uri = `/orders-api/orders/${orderId}/available-delivery-methods`;
+            const action = async () => await axios.get(uri);
+            await authorizedRequestHandler(action,
+                {
+                    status: 200,
+                    callback: result => {
+                        setState({
+                            loading: false,
+                            deliveryMethods: result.data
+                        });
+                    }
+                }
+            );
+        };
+        fetch();
+    }, []);
+
+
+    const onSubmit = async event => {
+        event.preventDefault();
+
+        const dataJson = getFormDataJsonFromEvent(event);
+        const uri = `/orders-api/orders/${orderId}/delivery-info`;
+        const action = async () => await axios.put(uri, dataJson);
+
+        await authorizedRequestHandler(action,
+            {
+                status: 200,
+                callback: () => {
+                    history.push(`/user/orders/${orderId}/summary`);
+                }
+            }
+        );
+    };
+
+    if (state.loading) return <></>
+
+    if (!state.deliveryMethods?.length) return <>
+        <h3>Error. No delivery methods to choose...</h3>
+    </>
+
+    return <>
+        <h3>Delivery address</h3>
+
+        <OrderDeliveryAddressForm
+            deliveryAddress={deliveryAddress}
+            setDeliveryAddress={setDeliveryAddress}
+            onSubmit={onSubmit}
+        >
+            <DeliveryAddressesModal
+                setDeliveryAddressState={setDeliveryAddress}
+            />
+
+            <DeliveryMethodsSection
+                deliveryMethods={state.deliveryMethods}
+                defaultSelected={state.deliveryMethods[0].name}
+            />
+
+            <button className="btn btn-success btn-block mt-4">
+                Finalize order
+            </button>
+        </OrderDeliveryAddressForm>
+    </>
+};
+
+export default FillDeliveryInfoPage;
