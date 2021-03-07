9698a4ad8d571c1156f83a030f77c2a89e21774d
Kakaranish <stasiekgruz@gmail.com>
2021-03-07 21:37:28 +0100

    + removed offer forms

--- src/routeAggregators/OfferRoutes.js
+++ src/routeAggregators/OfferRoutes.js
@@ -1,9 +1,8 @@
 import OfferPage from 'pages/OfferPage/OfferPage';
 import OffersPage from 'pages/OffersPage/OffersPage';
-import CreateOfferDraftPage from 'pages/User/Offers/CreateOfferPage/StageOne/CreateOfferDraftPage';
-import EditOfferDraftStageTwoPage from 'pages/User/Offers/CreateOfferPage/StageTwo/EditOfferDraftStageTwoPage';
 import EditActiveOfferPage from 'pages/User/Offers/EditActiveOfferPage/EditActiveOfferPage';
-import EditOfferDraftPage from 'pages/User/Offers/EditOfferDraftPage/EditOfferDraftPage';
+import CreateOfferDraftPage from 'pages/User/Offers/OfferDraft/CreateOfferDraftPage/CreateOfferDraftPage';
+import EditOfferDraftPage from 'pages/User/Offers/OfferDraft/EditOfferDraftPage/EditOfferDraftPage';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AuthorizedOnlyRoute from 'routeTypes/AuthorizedOnlyRoute';
@@ -12,8 +11,7 @@ const OfferRoutes = () => <>
     <Switch>
         <Route exact path='/offers' component={OffersPage} />
         <AuthorizedOnlyRoute exact path='/offers/create' component={CreateOfferDraftPage} />
-        <AuthorizedOnlyRoute exact path='/offers/create/draft/:id/stage/1' component={EditOfferDraftPage} />
-        <AuthorizedOnlyRoute exact path='/offers/create/draft/:id/stage/2' component={EditOfferDraftStageTwoPage} />
+        <AuthorizedOnlyRoute exact path='/offers/create/draft/:id' component={EditOfferDraftPage} />
         <AuthorizedOnlyRoute exact path='/offers/:id/edit' component={EditActiveOfferPage} />
         <Route exact path='/offers/:id' component={OfferPage} />
     </Switch>
